46a82a65917efb12464f0068f8db00dc645cad8a
gernot-ohner <gernot.ohner@gmail.com>
2021-01-29 14:14:33 +0100

    adds meal plan to MainView

--- src/main/java/dev/ohner/shoppy/ui/view/MainView.java
+++ src/main/java/dev/ohner/shoppy/ui/view/MainView.java
@@ -1,34 +1,59 @@
 package dev.ohner.shoppy.ui.view;
 
-import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.router.Route;
+import dev.ohner.shoppy.backend.model.MealPlan;
+import dev.ohner.shoppy.backend.persistence.model.Ingredient;
+import dev.ohner.shoppy.backend.persistence.model.Recipe;
 import dev.ohner.shoppy.backend.service.MailService;
+import dev.ohner.shoppy.backend.service.MealPlanGeneratorService;
 import dev.ohner.shoppy.ui.MainLayout;
 
 @Route(value = "", layout = MainLayout.class)
 public class MainView extends VerticalLayout {
 
     private final MailService mailService;
+    private final MealPlanGeneratorService mealPlanGeneratorService;
 
     private final EmailField emailField = new EmailField();
-    private final Button sendPerMailButton = new Button("Send this mealplan by mail");
+    private final Button sendPerMailButton = new Button("Send me the shopping list for this meal plan by mail");
+    private final HorizontalLayout emailLayout = new HorizontalLayout(emailField, sendPerMailButton);
 
+    private final Button generateMealPlanButton = new Button("Generate a new Meal Plan!");
 
-    public MainView(MailService mailService) {
+    private final H2 recipeHeader = new H2("Recipes");
+    private final Grid<Recipe> recipeGrid = new Grid<>(Recipe.class);
+    private final VerticalLayout recipeLayout = new VerticalLayout(recipeHeader, recipeGrid);
+
+    private final H2 ingredientHeader = new H2("Ingredients");
+    private final Grid<Ingredient> ingredientGrid = new Grid<>(Ingredient.class);
+    private final VerticalLayout ingredientLayout = new VerticalLayout(ingredientHeader, ingredientGrid);
+
+    public MainView(MailService mailService, MealPlanGeneratorService mealPlanGeneratorService) {
         this.mailService = mailService;
+        this.mealPlanGeneratorService = mealPlanGeneratorService;
 
-        sendPerMailButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-        sendPerMailButton.addClickShortcut(Key.ENTER);
+        sendPerMailButton.addThemeVariants(ButtonVariant.LUMO_SUCCESS);
         sendPerMailButton.addClickListener(e -> sendMail());
 
         emailField.setPlaceholder("email@example.com");
 
-        add(emailField, sendPerMailButton);
+        generateMealPlanButton.addThemeVariants(ButtonVariant.LUMO_ICON);
+        generateMealPlanButton.addClickListener(event -> showNewMealPlan());
+
+        recipeLayout.setVisible(false);
+        ingredientLayout.setVisible(false);
+
+        add(generateMealPlanButton);
+        add(recipeLayout);
+        add(ingredientLayout);
+        add(emailLayout);
     }
 
     public void sendMail() {
@@ -40,6 +65,21 @@ public class MainView extends VerticalLayout {
         remove(emailField, sendPerMailButton);
         add(new H2("Sent!"));
 
+    }
+
+    public void showNewMealPlan() {
+        final var mealPlan = mealPlanGeneratorService.generate();
+        configureMealPlanDisplay(mealPlan);
+    }
+
+    private void configureMealPlanDisplay(MealPlan mealPlan) {
+        recipeGrid.setColumns("id", "name");
+        recipeGrid.setItems(mealPlan.getRecipes());
+        recipeLayout.setVisible(true);
+
+        ingredientGrid.setColumns("id", "name");
+        ingredientGrid.setItems(mealPlan.getIngredients());
+        ingredientLayout.setVisible(true);
     }
 
 }
